e5a6859afe650347ec0b97ffb200e8952cd9657b
daniel <dany20082011@gmail.com>
2026-03-13 01:59:21 +0200

    setting better auth

---
+++ src/server/auth/index.ts
@@ -0,0 +1,33 @@
+import { db } from "@/lib/db";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma"
+
+
+export const auth = betterAuth({
+  database: prismaAdapter(db, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7,
+    updateAge: 60 * 60 * 24,
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 5,
+    },
+  },
+  trustedOrigins: [process.env.BETTER_AUTH_URL!],
+});
+
+export type Session = typeof auth.$Infer.Session;
