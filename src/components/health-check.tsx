2c3c345cc8acdbf86d9e00915c2ad26f41cbfb73
daniel <dany20082011@gmail.com>
2026-03-13 01:59:40 +0200

    adding ui components

---
+++ src/components/health-check.tsx
@@ -0,0 +1,21 @@
+import { trpc } from "@/lib/trpc";
+import { Badge } from "./ui/badge";
+import { Skeleton } from "./ui/skeleton";
+
+export function HealthCheck() {
+    const {data, isLoading, error } = trpc.health.useQuery();
+
+    if(isLoading) {
+        return <Skeleton className="h-6 w-24" />
+    }
+
+    if (error) {
+      return <Badge variant="destructive">API Error</Badge>;
+    }
+
+    return (
+        <Badge variant="secondary">
+            API: {data?.status} {data?.timestamps}
+        </Badge>
+    )
+}
